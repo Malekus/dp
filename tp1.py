dd34fcae110afa6bacaec45dd7a4a6b0f78595c6
Malekus <malekus@outlook.fr>
2019-01-21 19:04:06 +0100

    update tp1

--- tp1.py
+++ tp1.py
@@ -2,41 +2,57 @@ from sklearn.datasets.samples_generator import make_blobs
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from sklearn.decomposition import PCA
 
 np.set_printoptions(threshold=np.nan)
 
 x, y = make_blobs(n_samples=100, centers=2, n_features=2, random_state=0)
-
+x = np.column_stack((x, np.ones(x.shape[0])))
 print(x, np.array([k if k == 1 else -1 for k in y]))
 
 plt.figure()
 plt.scatter(np.array(x + np.column_stack((y,y)))[:,0], np.array(x - np.column_stack((y,y)))[:,1], c=np.array([k if k == 1 else -1 for k in y]))
 plt.show()
 
-def err_adal(d, w, x):
-    return d - w * x
+W = [random.uniform(0.0, 1.0) for _ in range(x.shape[1])]
 
-def adapt_adal(et, x):
-    return -2 * et * x
+def hasardW(x):
+    return np.array([random.uniform(0.0, 1.0) for _ in range(x.shape[1])])
 
-
-def adaline(x, y, pas):
-    W = np.array([[random.uniform(0.0, 1.0)] for _ in y])
-    
-    ecartTab = err_adalTab(y, W[:, -1], x)
-    adaptTab = adapt_adal_Tab(y, np.array(ecartTab)[-1], x)
-    
-    W.append(W[:, -1] - pas * adapt_adal_Tab(y, W, x))
-    
-    return W
+def err_dal(w, x, y):
+    return y - np.dot(x, w)
 
 
 
-print(adaline(x, y, 0.1))
+def grad_adal(w, x, et):
+    return -2 * np.dot(et, x)
+
+def adapt_adal(w, ra, pas):
+    return w - pas * ra
 
 
-def err_adalTab(y, W, x):
-    return np.array([toto[:, -1] - W[:,-1] * x[:,0], toto[:, -1] - W[:,-1] * x[:,0]])
+def adaline(x, y, tauxErreur=0.5, pas=0.1):
+    W = hasardW(x)
+    WW = []
+    while(np.any(np.absolute(err_dal(W, x, y)) < 0.5)):
+        WW.append(W)
+        et = err_dal(W, x, y)
+        ra = grad_adal(W, x, et)
+        W = adapt_adal(W, ra, pas)
+    return np.array(WW)
 
-def adapt_adal_Tab(y, et, x):
-    return np.array([-2 * et[0] * x[:,0], -2 * et[1] * x[:,1]])
+adaline = []
+adaline = adaline(x, y, pas=0.1)
+
+len(adaline)
+
+pca = PCA(n_components=1).fit_transform(adaline[0])
+
+plt.figure()
+plt.plot(range(adaline[1]), pca[:,0])
+plt.show()
+
+W = hasardW(x)
+True == np.all(np.power(W, 2) < 0.5)
+
+np.abs(err_dal(W, x, y))
